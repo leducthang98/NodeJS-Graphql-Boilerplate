73bd4b44c1dd647f646e325551394d78ce22aec8
LeDucThang <leducthang98@gmail.com>
2022-02-19 18:13:19 +0700

    setup old version prisma

--- src/schema.js
+++ src/schema.js
@@ -1,7 +1,8 @@
-import { makeSchema } from 'nexus'
+import  {makePrismaSchema}  from 'nexus-prisma'
 import { join } from 'path'
 import { resolver } from './resolver'
-export const schema = makeSchema({
+
+export const schema = makePrismaSchema({
   types: resolver,
   outputs: {
     typegen: join(__dirname, './generated/nexus-typegen.ts'),
